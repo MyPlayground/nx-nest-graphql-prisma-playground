0e4150c77a62700c14ad968f022739ffb96d9343
MyPlayground <57849991+MyPlayground@users.noreply.github.com>
2023-02-01 13:53:23 +0000

    カスタムエラーを返すテスト

--- apps/backend/src/app/app.resolver.ts
+++ apps/backend/src/app/app.resolver.ts
@@ -1,4 +1,5 @@
 import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Prisma } from '@prisma/client';
 import { PrismaService } from 'nestjs-prisma';
 import { User, UserCreateInput } from './models';
 import { OffsetPaginationInput } from './models/offset-pagination.input';
@@ -22,6 +23,18 @@ export class AppResolver {
 
   @Mutation(() => User)
   async createUser(@Args('data') data: UserCreateInput): Promise<User> {
-    return await this.prisma.user.create({ data });
+    try {
+      return await this.prisma.user.create({ data });
+    } catch (e: unknown) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === 'P2002') {
+          console.log(
+            'There is a unique constraint violation, a new user cannot be created with this email'
+          );
+          throw new Error('かすたむえらー');
+        }
+      }
+      throw e;
+    }
   }
 }
